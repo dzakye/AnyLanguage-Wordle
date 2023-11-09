b17e2b9db540516645ac27481ac7fdb15e67982d
dzakye <150271646+dzakye@users.noreply.github.com>
2023-11-09 13:16:21 +0100

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -15,6 +15,6 @@ export const CONFIG = {
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
   startDate: 'January 1, 2022 00:00:00', // what date and time to start your game from
   defaultLang: 'arp', // the default interface language
-  availableLangs: ['arp','fr','en', 'es', 'sw', 'zh'], // the options available to the user for translation languages
+  availableLangs: ['arp','f','en', 'es', 'sw', 'zh'], // the options available to the user for translation languages
   escapeSpecialCharacters: true, // whether to escape all characters in the orthography.
 }
